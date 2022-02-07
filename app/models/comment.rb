fc0ba01846c4a329cb2f828dc77b635a81206eae
Jiting <jiting@jtcat.com>
2022-02-08 04:46:35 +0800

    Feat: style post show page and nested comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :commentable, polymorphic: true
   belongs_to :parent, optional: true, class_name: "Comment"
+  has_many :comments, foreign_key: :parent_id, dependent: :destroy
 
-  store_accessor :payload, :raw_comment
+  store_accessor :payload, :raw_comment, :type
 
   def comments
     Comment.where(commentable: commentable, parent_id: id)
