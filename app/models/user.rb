70edd1c1c970784ef970c54e0489164425f459dd
Jiting <jiting@jtcat.com>
2022-12-27 21:11:57 +0800

    add github oauth (wip)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
-         :recoverable, :rememberable, :validatable #, :confirmable, :lockable
+         :recoverable, :rememberable, :validatable,  # :confirmable, :lockable
+         :omniauthable, omniauth_providers: %i[github]
 
   has_many :posts
   has_many :comments
